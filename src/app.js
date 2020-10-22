ed0f8a81541cad01d1a61876be7854ccf4a68451
Andrew Coleburn <arcoleburn@gmail.com>
2020-10-22 19:59:14 -0400

    init commit

--- src/app.js
+++ src/app.js
@@ -6,10 +6,23 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const winston = require('winston');
 
 const app = express();
 
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [new winston.transports.File({ filename: 'info.log' })],
+});
+if (NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
 
 app.use(morgan(morganOption));
 app.use(helmet());
@@ -29,4 +42,33 @@ app.use(function errorHandler(error, req, res, next) {
   }
   res.status(500).json(response);
 });
+
+const cards = [
+  {
+    id: 1,
+    title: 'Task one',
+    content: 'this is card one',
+  },
+];
+
+const lists = [
+  {
+    id: 1,
+    header: 'List One',
+    cardIds: [1],
+  },
+];
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  // move to the next middleware
+  next();
+});
+
 module.exports = app;
